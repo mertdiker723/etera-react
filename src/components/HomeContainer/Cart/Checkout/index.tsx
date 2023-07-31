dd6cc3e469fa098d7184b04d837fa5729abc88ae
Mert Diker <mertdiker723@gmail.com>
2023-07-31 18:57:18 +0300

    cart plus added

--- src/components/HomeContainer/Cart/Checkout/index.tsx
+++ src/components/HomeContainer/Cart/Checkout/index.tsx
@@ -1,7 +1,7 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Material UI
-import { Card, CardActions, CardContent, Button, Typography } from '@mui/material';
+import { Card, CardActions, CardContent, Button, Typography, ToggleButtonGroup, ToggleButton, Grid } from '@mui/material';
 
 // Types
 import { InitialStateType } from '../../../../core/redux/reducer/cart/initialState';
@@ -9,7 +9,16 @@ import { InitialStateType } from '../../../../core/redux/reducer/cart/initialSta
 
 const Checkout = () => {
     const cartItem = useSelector<{ cartItem: InitialStateType }>((item) => item.cartItem) as InitialStateType[];
-    
+    const dispatch = useDispatch()
+
+    const handleChange = (event: React.MouseEvent<HTMLElement>, data: InitialStateType) => {
+        const { id, name, price, count } = data;
+        if (event.currentTarget.id === "plus") {
+            dispatch({ type: "CART_ADD", payload: { id, name, price, count } })            
+            return;
+        }        
+    };
+
     return (
         <Card>
             <CardContent>
@@ -18,25 +27,32 @@ const Checkout = () => {
                 </Typography>
                 {
                     cartItem.map(item => {
-                        const { id, name, price, count } = item;
+                        const { id, count, name, price } = item;
                         return (
-                            <Typography key={id} component="div">
-                                {name} - Price {price * (count || 1)} - Count: {count}
-                            </Typography>
+                            <Grid container sx={{ mt: 1 }}>
+                                <Grid item xs={6}>
+                                    {name}
+                                    <br />
+                                    {price * (count || 1)} ₺
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <ToggleButtonGroup
+                                        value={id}
+                                        exclusive
+                                        key={id}
+                                        onChange={handleChange}
+                                        aria-label="Platform"
+                                    >
+                                        <ToggleButton id="minus" value={item}>-</ToggleButton>
+                                        <ToggleButton disabled value="price">{count}</ToggleButton>
+                                        <ToggleButton id="plus" value={item}>+</ToggleButton>
+                                    </ToggleButtonGroup>
+                                </Grid>
+
+                            </Grid>
                         )
                     })
                 }
-                <Typography variant="h5" component="div">
-                    ntesting
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    adjective
-                </Typography>
-                <Typography variant="body2">
-                    well meaning and kindly.
-                    <br />
-                    {'"a benevolent smile"'}
-                </Typography>
             </CardContent>
             <CardActions>
                 <Button size="small">Learn More</Button>
