16a400df0c0d4149d4d46a057473b320f237030c
Mert Diker <mertdiker723@gmail.com>
2023-08-02 21:30:00 +0300

    brand modal removed

--- src/components/HomeContainer/SideBar/index.tsx
+++ src/components/HomeContainer/SideBar/index.tsx
@@ -1,14 +1,10 @@
 import { Container } from "@mui/material"
-import Brands from "./Brands"
-import Model from "./Model"
 import SortBy from "./SortBy"
 
 const SideBar = () => {
     return (
         <Container disableGutters sx={{ mt: 4 }}>
             <SortBy />
-            <Brands />
-            <Model />
         </Container>
     )
 }
